e5291d51738cd908aee4edfc35cf4b8ac72a8d05
nattawat <frank.nattawat@hotmail.com>
2024-05-08 19:07:19 +0700

    feat:finish

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -13,11 +13,12 @@ function HomePage() {
       <div className="first-container bg-gradient-to-b from-[#070707] to-[#dd7ab1] h-[300px] rounded-b-[70px] ">
         <img
           src="/pic/jj1.png"
-          className=" relative z-10 top-[50px] left-[140px] h-[300px]"
+          className=" relative z-10 top-[130px] left-[190px] h-[270px]"
         />
+
         <img
           src="/pic/rainbow.png"
-          className=" relative z-10 left-[50px] bottom-[250px] h-[50px] animate-bounce"
+          className=" relative z-10 left-[50px] bottom-[200px] h-[50px] animate-bounce"
         />
         <img
           src="/pic/star.png"
@@ -25,22 +26,26 @@ function HomePage() {
         />
         <img
           src="/pic/heart.png"
-          className=" relative z-10 bottom-[20px] left-[30px] h-[50px] animate-bounce"
+          className=" relative  z-20 top-[50px] left-[10px] h-[60px] animate-bounce "
         />
         <img
           src="/pic/umbrella.png"
-          className=" relative z-10 top-[50px] left-[340px] h-[50px] animate-bounce"
+          className=" relative z-20 top-[50px] left-[320px] h-[50px] animate-bounce"
         />
         <img
           src="/pic/smile1.png"
           className=" relative z-10 top-[70px] left-[140px] h-[50px] animate-bounce"
         />
+        <img
+          src="/pic/jj2.png"
+          className=" absolute z-10 top-[150px] left-[50px] h-[250px]"
+        />
       </div>
-      <div className="curve bg-[#070707] h-[200px] w-[250px] absolute top-[150px] left-[20%] rounded-t-full z-0"></div>
-      <div className="secound-container bg-gradient-to-t from-[#070707] to-[#dd7ab1] h-[400px] rounded-t-[70px] relative top-[50px] ">
+
+      <div className="secound-container bg-gradient-to-t from-[#070707] to-[#dd7ab1] h-[400px] rounded-t-[70px] relative top-[50px] z-0">
         <Accordion
           allowToggle
-          className="w-[270px] absolute left-[80px] top-[100px]"
+          className="w-[270px] absolute left-[60px] top-[90px]  z-10"
         >
           <AccordionItem className="bg-[#d64783] rounded-md  border-2">
             <h2>
@@ -55,6 +60,7 @@ function HomePage() {
           </AccordionItem>
         </Accordion>
       </div>
+      <div className="curve bg-[#070707] h-[180px] w-[360px] absolute top-[220px] left-[10px] rounded-full z-0"></div>
     </div>
   );
 }
